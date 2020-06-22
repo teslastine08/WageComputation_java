8170fc6fa4ab851a7f66a936d7fe9bbd139c109a
teslastine08 <teslastine08@gmail.com>
2020-06-22 20:38:48 +0530

    UC1: Attandance

--- wage.java
+++ wage.java
@@ -1,10 +1,17 @@
+import java.util.Random;
 public class wage
-
 {
 
 	public static void main(String args[])
 	{
-	System.out.println("Welcome to wage computation");
+	 System.out.println("Welcome to wage computation");
+         
+         Random rand = new Random();
+
+         int attan = rand.nextInt(30);
+
+         System.out.println("Attandance: "+attan);
+
 	}
 
 }
