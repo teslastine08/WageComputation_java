1bd47b6158a02514278b7468c23c13df00686c45
teslastine08 <teslastine08@gmail.com>
2020-06-22 20:51:55 +0530

    UC2: Daily Wage

--- wage.java
+++ wage.java
@@ -4,13 +4,18 @@ public class wage
 
 	public static void main(String args[])
 	{
+	
+	int wph = 20;
+	int fdh = 8;
 	 System.out.println("Welcome to wage computation");
-         
+
          Random rand = new Random();
 
          int attan = rand.nextInt(30);
 
          System.out.println("Attandance: "+attan);
+
+	 System.out.println("Daily employee wage: Rs."+(wph*fdh));
 
 	}
 
