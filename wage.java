b51eae52ae460be5dad83bd5deec6635dbe8e8d8
teslastine08 <teslastine08@gmail.com>
2020-06-23 10:13:15 +0530

    UC4: Case Added

--- wage.java
+++ wage.java
@@ -2,52 +2,29 @@ import java.util.Random;
 import java.util.Scanner;
 public class wage
 {
+public static final int is_part = 1;
+public static final int is_full = 2;
+public static final int  emp_rate_per_hour =20;
 
 	public static void main(String args[])
 	{
 	
-	 int wph = 20;
-	 int fdh = 8;
-	 int parthr = 8;
-	 int num;
-
-	 Scanner in = new Scanner(System.in);
-
-	 System.out.println("Welcome to wage computation");
-
-
-
-
-
-         Random rand = new Random();
-
-         int attan = rand.nextInt(30);
-
-         System.out.println("Attandance: "+attan);
-
-
-
-	 System.out.println("Daily employee wage: Rs."+(wph*fdh));
-
-	 System.out.println("Employee monthly wage: Rs."+((wph*fdh)*attan));
-
-
-
-	System.out.println("Enter part time Days: ");
-        num = in.nextInt();
-
-	switch (num)
+	 int emphrs = 0;
+	 int empwage = 0;
+	int empcheck = (int) Math.floor(Math.random() *10) % 3;
+	switch (empcheck)
 	{
-	case 1:
-	if(num < 30)
-	System.out.println("Part time wage: Rs."+(num*parthr*wph));
-	break;
-
+	case is_part:
+		emphrs = 4;
+		break;
+	case is_full:
+		emphrs = 8;
+		break;
 	default:
-	System.out.println("Data provided wrong");
-	break;
-
+		emphrs = 0;
 	}
+	empwage = emphrs * emp_rate_per_hour;
+	System.out.println("empwage: Rs."+empwage);
 	}
 
 }
